e6bb75a4f4a649a40f14254e8c31da8487ee916c
amoghmanoranjith <amoghmanoranjith@gmail.com>
2026-06-30 19:14:50 +0530

    Refactor: changed main in accordance to other services

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
-import(
-	"rradar/model"
+import (
 	"rradar/db"
-	"rradar/cmd"
+	"rradar/model"
+	"rradar/service"
 	"time"
 )
 
-func main(){
+func main() {
 	repo, err := db.New("posts.db")
 	if err != nil {
 		panic(err)
@@ -17,10 +17,19 @@ func main(){
 	ticker := time.NewTicker(time.Minute)
 	defer ticker.Stop()
 	// 1st minute subreddit 1
-	// after that subreddit 1 is done wait for 1 minute run for 2nd subreddit 
+	// after that subreddit 1 is done wait for 1 minute run for 2nd subreddit
 	for i := 0; i < len(model.Subreddits); i++ {
 		subreddit := model.Subreddits[i]
-		cmd.Process(repo, subreddit)
+		feed := service.Scrape(subreddit)
+
+		// repo.UpdatePost with the latest one among all
+		latest := feed.Entries[0]
+		err = repo.UpdatePost(feed.Subreddit, latest.Author, latest.Published)
+		if err != nil {
+			panic(err)
+		}
+
+		// process this dataArray
 		time.Sleep(time.Minute)
 		if i == len(model.Subreddits)-1 {
 			i = -1 // reset to start
