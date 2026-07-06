ecd115ed262a45c68f24b995cbf1ae2a5eeb41ca
amoghmanoranjith <amoghmanoranjith@gmail.com>
2026-07-06 17:52:53 +0530

    Refactor: now uses post and classifiedPost types

--- domain/classifier.go
+++ domain/classifier.go
@@ -1,10 +1,9 @@
 package domain
 
 import (
-	modelLLM "rradar/model/llm"
-	modelXML "rradar/model/xml"
+	"rradar/model"
 )
 
 type Classifier interface{
-	Classify(entry modelXML.Entry)(entryRelevance modelLLM.Entry, error error)
+	Classify(post model.Post)( model.ClassifiedPost, error)
 }
