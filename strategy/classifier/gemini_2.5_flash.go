a8117bd6459dd1c700fca96552bc10e6d10684a7
amoghmanoranjith <amoghmanoranjith@gmail.com>
2026-07-10 21:47:39 +0530

    Refactor: added better logs

--- strategy/classifier/gemini_2.5_flash.go
+++ strategy/classifier/gemini_2.5_flash.go
@@ -4,9 +4,10 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
-	clientHTTP "rradar/http"
 	adapter "rradar/adapter/classifier"
+	clientHTTP "rradar/http"
 	"rradar/model"
 )
 
@@ -33,20 +34,45 @@ func (g Gemini25Flash) Classify(post model.Post) (model.ClassifiedPost, error) {
 		bytes.NewReader(data),
 	)
 	if err != nil {
+		slog.Error(
+			"failed to create post request",
+			"component", "Gemini25Flash",
+			"operation", "Classify",
+			"cause", "http.NewRequest",
+			"error", err,
+		)
 		return model.ClassifiedPost{}, err
 	}
-	
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("X-goog-api-key", g.apiKey)
 	
 	resp, err := clientHTTP.Client.Do(req)
 	if err != nil {
+		slog.Error(
+			"failed to make post request to gemini 2.5 flash api",
+			"component", "Gemini25Flash",
+			"operation", "Classify",
+			"cause", "clientHTTP.Client.Do",
+			"error", err,
+		)
 		return model.ClassifiedPost{}, err
 	}
 	defer resp.Body.Close()
 	// read the data in response body, convert it to []byte
+	slog.Info(
+    "Gemini Flash response",
+    "status", resp.Status,
+    "status_code", resp.StatusCode,
+)	// read the data in response body, convert it to []byte
 	data, err = io.ReadAll(resp.Body)
 	if err != nil {
+		slog.Error(
+			"failed to read from raw response from gemini 2.5 flash api",
+			"component", "Gemini25Flash",
+			"operation", "Classify",
+			"cause", "io.ReadAll",
+			"error", err,
+		)
 		return model.ClassifiedPost{}, err
 	}
 	if resp.StatusCode != http.StatusOK {
