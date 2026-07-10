a8bb04e1cbdea27c93d5f43668c50b49cbee90b4
amoghmanoranjith <amoghmanoranjith@gmail.com>
2026-07-10 21:38:01 +0530

    Refactor: added mistral key

--- config/env_config.go
+++ config/env_config.go
@@ -3,24 +3,28 @@ package config
 import (
 	"fmt"
 	"os"
-
 	"github.com/joho/godotenv"
 )
 
-type Config struct{
+type Config struct {
 	GeminiAPIKey   string
+	MistralAPIKey  string
 	DiscordWebhook string
 }
 
-func Load() (*Config, error){
+func Load() (*Config, error) {
 	// loads .env file variables into process memory
 	_ = godotenv.Load()
 	cfg := &Config{
-		GeminiAPIKey: os.Getenv("GEMINI_API_KEY"),
+		GeminiAPIKey:   os.Getenv("GEMINI_API_KEY"),
+		MistralAPIKey:  os.Getenv("MISTRAL_API_KEY"),
 		DiscordWebhook: os.Getenv("DISCORD_WEBHOOK"),
 	}
 	if cfg.GeminiAPIKey == "" {
 		return nil, fmt.Errorf("GEMINI_API_KEY missing")
+	}
+	if cfg.MistralAPIKey == "" {
+		return nil, fmt.Errorf("MISTRAL_API_KEY missing")
 	}
 	if cfg.DiscordWebhook == "" {
 		return nil, fmt.Errorf("DISCORD_WEBHOOK missing")
